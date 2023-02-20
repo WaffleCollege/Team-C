575188ba976410176168beed8a9cd507fa68ae62
Mahaut-s <mascal.mao14@gmail.com>
2023-02-20 20:30:32 +0100

    rooting

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('チームプロジェクト');
+    res.render('hello.ejs');
   });  
+
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
 
 app.listen(3000);
 
