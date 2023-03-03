4e8cd7b65f0216b6f8c3f286d6e7fd89f65d2e88
Mahaut-s <mascal.mao14@gmail.com>
2023-03-03 20:06:09 +0100

    connectMySQL

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
+//サーバーを立てる
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 
+//MySQLとの接続
+const mysql = require('mysql');
+
+//MySQLの接続情報
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'WaffleTeam_c2023',
+  database: 'teamproject'
+});
+
+//MySQLに接続できなかった時にエラーを表示する
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
+
+
+//ルーティング設定
 app.get('/', (req, res) => {
-    res.render('hello.ejs');
-  });  
+  connection.query(
+    'SELECT * FROM users',
+    (error, results) => {
+      console.log(results);
+      res.render('hello.ejs');
+    }
+  );
+});  
 
 app.get('/top', (req, res) => {
   res.render('top.ejs');
