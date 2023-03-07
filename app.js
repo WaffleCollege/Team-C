30d300a4b6f960c2ec64eb796221c5c25c1172e4
Mahaut-s <mascal.mao14@gmail.com>
2023-03-07 18:06:52 +0100

    login_succeeded

--- app.js
+++ app.js
@@ -1,57 +1,53 @@
 //サーバーを立てる
 const express = require('express');
+//MySQLとの接続
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
 //express-sessionの読み込み
 const session = require('express-session');
 
+
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
-
-//MySQLとの接続
-const mysql = require('mysql');
 
 //MySQLの接続情報
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'WaffleTeam_c2023',
-  database: 'teamproject'
-});
-
-//MySQLに接続できなかった時にエラーを表示する
-connection.connect((err) => {
-  if (err) {
-    console.log('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('success');
+  database: 'teamc'
 });
 
 app.use(
-  session({
+ session({
     secret: 'my_secret_key',
     resave: false,
     saveUninitialized: false,
   })
 );
 
+//app.use関数で常にログイン状態を確認
+app.use((req, res, next) => {
+  if (req.session.userId === undefined) {
+    console.log('ログインしていません');
+  } else {
+    console.log('ログインしています');
+  }
+  next();
+});
 
 //メインページのルーティング設定
 app.get('/', (req, res) => {
-  connection.query(
-    'SELECT * FROM users',
-    (error, results) => {
-      console.log(results);
-      res.render('top.ejs');
-    }
-  );
+  res.render('hello.ejs');
 });  
 
+
 //ユーザー登録画面のルーティング設定
-app.get('/signup', (req, res) => {
-  res.render('signup.ejs');
-});
+//app.get('/signup', (req, res) => {
+  //res.render('signup.ejs');
+//});
 
 //ログインページのルーティング設定
 app.get('/login', (req, res) => {
@@ -68,7 +64,7 @@ app.post('/login', (req, res) => {
         if (req.body.password === results[0].password){
           // ユーザーIDをセッション情報に保存
           req.session.userId = results[0].id;
-          res.redirect('/');
+          res.redirect('/main');
         } else {
           res.redirect('/login');
         }
@@ -77,6 +73,12 @@ app.post('/login', (req, res) => {
       }
     }
   );
+});
+
+
+//カレンダーページのルーティング設定
+app.get('/main', (req, res) => {
+  res.render('calendar.ejs');
 });
 
 app.listen(3000);
